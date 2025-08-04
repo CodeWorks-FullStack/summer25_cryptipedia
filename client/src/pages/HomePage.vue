f14a40818cad73b1ba6a3a33e0cbc4179a279e4d
Jeremy Fowler <jembyfowler@gmail.com>
2025-08-04 11:47:04 -0600

    getting cryptids

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,5 +1,19 @@
 <script setup>
+import { cryptidsService } from '@/services/CryptidsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
+onMounted(getCryptids)
+
+async function getCryptids() {
+  try {
+    await cryptidsService.getCryptids()
+  } catch (error) {
+    Pop.error(error)
+    logger.error('COULD NOT GET CRYPTIDS', error)
+  }
+}
 
 </script>
 
